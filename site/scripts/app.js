8135098809b96e0bbd8757772ffa1787feb16aaf
luis <flore.tec@gmail.com>
2015-04-25 17:12:38 -0500

    02. Post con ng repeat

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,4 +5,11 @@ modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
+		$scope.posts = 
+		["Post 1 Hola",
+		 "Post 2",
+		 "Post 3",
+		 "Post 4",
+		 "Post 5",
+		 "Post 6"];
 	}]);
