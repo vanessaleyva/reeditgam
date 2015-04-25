6d6bcf76b67c72fcb7fc930f803f8a470b8dec21
luis <flore.tec@gmail.com>
2015-04-25 17:23:09 -0500

    04. Agregar nuevos post staticos

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,6 +5,7 @@ modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
+		// Modelo
 		$scope.posts = 
 		[{title: "Post 1", upvotes: 5},
 		 {title: "Post 2", upvotes: 15},
@@ -12,4 +13,11 @@ modulo1.controller("mainCtrl",[
 		 {title: "Post 4", upvotes: 9},
 		 {title: "Post 5", upvotes: 3},
 		 {title: "Post 6", upvotes: 5}];
+
+		 // Metodo del controlador
+		 $scope.addPost = function(){
+		 	$scope.posts.push(
+		 		{title:"Nuevo Post",
+		 		 upvotes: 0});
+		 };
 	}]);
