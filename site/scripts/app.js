6037e881a68ae673d05d7466d335629fc6bf73df
luis <flore.tec@gmail.com>
2015-04-26 13:17:49 -0500

    01.Hola Mundo Angular

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,23 +1,39 @@
-console.log("#IvanR: Cargo app.js");
-var modulo1 = 
+console.log ("#Montse: Cargo app.js");
+var modulo1=
 	angular.module("reeditgam",[]);
 modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
-		// Modelo
 		$scope.posts = 
-		[{title: "Post 1", upvotes: 5},
-		 {title: "Post 2", upvotes: 15},
-		 {title: "Post 3", upvotes: 7},
-		 {title: "Post 4", upvotes: 9},
-		 {title: "Post 5", upvotes: 3},
-		 {title: "Post 6", upvotes: 5}];
+	   [{title: "Post 1", upvotes: 5},
+		{title: "Post 2", upvotes: 15},
+		{title: "Post 3", upvotes: 7},
+		{title: "Post 4", upvotes: 9},
+		{title: "Post 5", upvotes: 3},
+		{title: "Post 6", upvotes: 5}];
+		//Metodo del controlador
 
-		 // Metodo del controlador
-		 $scope.addPost = function(){
-		 	$scope.posts.push(
-		 		{title:"Nuevo Post",
-		 		 upvotes: 0});
-		 };
+		$scope.addPost = function(){
+			if (!$scope.title || $scope.title === "")
+			{
+				alert("NO permite postear titutlos vacios")
+				return;
+			}
+			$scope.posts.push(
+			{
+				title: $scope.title,
+				link: $scope.link,
+			 upvotes: 0
+			});
+			//Two-way data binding
+			$scope.title = "";
+			$scope.link = "";
+		};
+
+		$scope.incrementUpvotes = function(post){
+			post.upvotes += 1;
+		}
+		
+
 	}]);
