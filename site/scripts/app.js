b823acf1d1bb12ac099436aadd5f17ff0692f6da
luis <flore.tec@gmail.com>
2015-05-07 12:59:20 -0500

    09. Se agrega un servicio de tipo  factory para alamacenar los posts

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,18 +1,48 @@
 console.log("#IvanR: Cargo app.js");
 var modulo1 = 
 	angular.module("reeditgam",[]);
+
+//creando un servicio de tipo factory
+modulo1.factory('posts',[function(){
+	//cuerpo del factory llamado post
+	var o = {
+		posts : [
+		{
+		title: "post 1", upvotes: 15,
+		comments: [
+		{author:"karina", body:"Esto esta de pelos",
+		upvotes:3},
+        {author: "Gamaliel", body:"Esto es basura",
+        upvotes:0}]
+		
+	},
+{
+		title: "post 2", upvotes: 4,
+		comments: [
+		{author:"vane", body:"Esto esta asombroso",
+		upvotes:5},
+        {author: "anahi", body:"Esto es aburrido",
+        upvotes:1}]
+		
+	}
+
+
+	]
+};
+//retornando onjetos de datos persistentes
+return o;
+
+}]);
+
+//creando controlador	
 modulo1.controller("mainCtrl",[
-	'$scope',
-	function($scope){
+	'$scope','posts', //INYECTANDO FACTORY POST
+	function($scope, posts){
 		$scope.test = "Hola Angular";
-		// Modelo
-		$scope.posts = 
-		[{title: "Post 1", upvotes: 5},
-		 {title: "Post 2", upvotes: 15},
-		 {title: "Post 3", upvotes: 7},
-		 {title: "Post 4", upvotes: 9},
-		 {title: "Post 5", upvotes: 3},
-		 {title: "Post 6", upvotes: 5}];
+		// Modelo al cual se le asigna
+		//el resultado del factory
+		$scope.posts = posts.posts;
+		
 
 		 // Metodo del controlador
 		 $scope.addPost = function(){
